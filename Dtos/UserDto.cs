56b489c7203ffeb42808239f9b97c8b921bbadb5
hasbiyallah01 <oyebohm@gmail.com>
2024-03-11 09:12:41 +0100

    UserDtoUpdate

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -1,15 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MgqsPollApp.Models.Enums;
 
 namespace PollAppMGQS.Dtos
 {
     public class UserDto
     {
-        public string Username { get; set; } = default!;
         public string Email { get; set; } = default!;
         public string Password { get; set; } = default!;
         public string DisplayName { get; set; } = default!;
+
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public IFormFile ImageUrl { get; set; } = default!;
+        public Gender Gender { get; set; } = default!;
     }
 }
--- obj/Debug/net8.0/MgqsPollApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MgqsPollApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MgqsPollApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ab50e69b73011c719a68dd6bf9410b2a3005eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f2f85c64f35826ecaffce345ab42a621acf3e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MgqsPollApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MgqsPollApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
