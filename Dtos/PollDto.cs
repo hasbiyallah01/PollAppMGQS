3c26ce4942cc742991d4f902ec65af45abc8eb7a
hasbiyallah01 <oyebohm@gmail.com>
2024-03-11 08:34:15 +0100

    PollDto

---
+++ Dtos/PollDto.cs
@@ -0,0 +1,10 @@
+﻿using MgqsPollApp.Models.Entities;
+
+namespace MgqsPollApp.Dtos
+{
+    public class PollDto
+    {
+        public DateTime Deadline { get; set; }
+        public string Question { get; set; } = default!;
+    }
+}
