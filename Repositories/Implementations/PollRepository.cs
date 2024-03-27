8ba3c0e27b50980dc19e9ea61ceb2cd6319411aa
Odusanya Ibrahim O <odusanyaibrahim2001@gmail.com>
2024-03-27 00:45:19 -0700

    Initial changes to PollRepository

--- Repositories/Implementations/PollRepository.cs
+++ Repositories/Implementations/PollRepository.cs
@@ -1,6 +1,86 @@
-﻿namespace MgqsPollApp.Repositories.Implementations
+﻿using MgqsPollApp.Context;
+using MgqsPollApp.Models.Entities;
+using static MgqsPollApp.Models.Entities.User;
+using MgqsPollApp.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Net;
+
+namespace MgqsPollApp.Repositories.Implementations
 {
-    public class PollRepository
+    public class PollRepository : BaseRepository<Poll>, IPollRepository
     {
+        public PollRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Poll> Get(int id)
+        {
+            var poll = await _context.Set<Poll>()
+                    .Include(a => a.User)
+                    .Include(a => a.Deadline)
+                    .Include(a => a.ChatRoom)
+                    .ThenInclude(a => a.ChatRoomUsers)
+                    .Include(a => a.CreatedAt)
+                .SingleOrDefaultAsync(a => a.Id == id);
+
+            return poll;
+        }
+
+        public async Task<Poll> Get(Expression<Func<Poll, bool>> predicate)
+        {
+            var poll = await _context.Set<Poll>()
+                    .Include(a => a.User)
+                    .Include(a => a.Deadline)
+                    .Include(a => a.ChatRoom)
+                    .ThenInclude(a => a.ChatRoomUsers)
+                    .Include(a => a.CreatedAt)
+                .SingleOrDefaultAsync(predicate);
+
+            return poll;
+        }
+
+        public async Task<ICollection<Poll>> GetAll()
+        {
+            var polls = await _context.Set<Poll>()
+                    .Include(a => a.User)
+                    .Include(a => a.Deadline)
+                    .Include(a => a.ChatRoom)
+                    .ThenInclude(a => a.ChatRoomUsers)
+                    .Include(a => a.CreatedAt)
+                .ToListAsync();
+
+            return polls;
+        }
+
+        public async Task<ICollection<Poll>> GetSelected(List<int> ids)
+        {
+            var polls = await _context.Set<Poll>()
+                    .Include(a => a.User)
+                    .Include(a => a.Deadline)
+                    .Include(a => a.ChatRoom)
+                    .ThenInclude(a => a.ChatRoomUsers)
+                    .Include(a => a.CreatedAt)
+                .Where(a => ids.Contains(a.Id))
+                .ToListAsync();
+
+            return polls;
+        }
+
+        public async Task<ICollection<Poll>> GetSelected(Expression<Func<Poll, bool>> predicate)
+        {
+            var polls = await _context.Set<Poll>()
+                    .Include(a => a.User)
+                    .Include(a => a.Deadline)
+                    .Include(a => a.ChatRoom)
+                    .ThenInclude(a => a.ChatRoomUsers)
+                    .Include(a => a.CreatedAt)
+                .Where(predicate)
+                .ToListAsync();
+
+            return polls;
+        }
     }
 }
